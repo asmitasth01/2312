c441ac836c025c85dfb771b5966239a4a1a996eb
asmitasth01 <asmitasth01@gmail.com>
2016-12-11 17:03:25 -0600

    Update px_axs4591.s

--- px_axs4591.s
+++ px_axs4591.s
@@ -80,20 +80,15 @@ _POW:
 	BL  _scanf              @ branch to scanf procedure with return
 	MOV R3, R0             @ move return value R0 to FPU register S1
 	MOV R0, #0              @ initialze index variable
-	@VMOV S3, R0
-	@VCVT.F32.U32 S3, S3     @ convert unsigned bit representation to single float
-	@MOV R8, R0
-	@VCVT.F32.U32 S1, S1     @ convert unsigned bit representation to single float
 	CMP R0, R3
+	VMOV S1, #2
 	BEQ _POWER_DONE
-	VMUL.F32 S2, S0, S0     @ compute S2 = S0 * S0
+	VMUL.F32 S2, S0, S1     @ compute S2 = S0 * S0
         VCVT.F64.F32 D4, S2     @ covert the result to double precision for printing
         VMOV R1, R2, D4         @ split the double VFP register into two ARM registers
 	ADD R0, R0, #1          @ increment index
+        B _POW           @ branch to next loop iteration
 	 BL  _printf_result      @ print the result
-	@VADD.F32 S3, S3,#1
-        @B _POW           @ branch to next loop iteration
-	@BL  _printf_result      @ print the result
          B main
 _POWER_DONE:
 	MOV R9, #0

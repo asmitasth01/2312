2302d7818c208e9531543defcac16406ce74f1dc
asmitasth01 <asmitasth01@gmail.com>
2016-12-11 19:15:06 -0600

    Update px_axs4591.s

--- px_axs4591.s
+++ px_axs4591.s
@@ -80,19 +80,19 @@ _beforePOW:
 	BL  _scanf              @ branch to scanf procedure with return
 	MOV R3, R0             @ move return value R0 to FPU register S1
 	SUB R3, R3, #1
-	MOV R0, #1              @ initialze index variable
+	MOV R0, #0              @ initialze index variable
 	VMOV S1, S0
 	
 _POW:
-	CMP R0, R3
+	@CMP R0, R3
 	@MOV R1, #2
 	@VMOV S1, R1
-	BEQ _POWER_DONE
+	@BEQ _POWER_DONE
 	VMUL.F32 S1, S0, S1     @ compute S1 = S0 * S1
         @VCVT.F64.F32 D4, S2     @ covert the result to double precision for printing
         @VMOV R1, R2, D4         @ split the double VFP register into two ARM registers
-	ADD R0, R0, #1          @ increment index
-        BL _POW           @ branch to next loop iteration
+	@ADD R0, R0, #1          @ increment index
+        @BL _POW           @ branch to next loop iteration
 	 
 _POWER_DONE:
 	@MOV R9, #0
